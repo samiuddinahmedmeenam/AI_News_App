f177c14b9fffb8a6f85058ec37314ca772c6d4b1
Samiuddin AHmed <samiuddinahmed88730@gmail.com>
2026-04-12 10:02:42 -0400

    Created Article class and tested its output

---
+++ News_App/Article.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Article
+{
+	public string Title { get; set; }
+	public string Description { get; set; }
+	public string Url { get; set; }
+}
+
+
--- News_App/Program.cs
+++ News_App/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Fetching news...");
+
+Article article = new Article();
+
+article.Title = "Breaking News: C# 12 Released!";
+article.Description = "The latest version of C# has been released, bringing new features and improvements to the language.";
+article.Url = "https://www.example.com/news/csharp-12-released";
+
+Console.WriteLine($"Title: {article.Title}");
+Console.WriteLine($"Description: {article.Description}");
+Console.WriteLine($"URL: {article.Url}");
