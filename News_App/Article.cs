74da55c75b0d39810d19a8e8ac064cb982e3a822
Samiuddin AHmed <samiuddinahmed88730@gmail.com>
2026-04-12 10:20:22 -0400

    Created article class and initialized objects in it. Then created a list of articles with fake news and printed all with for loop

--- News_App/Article.cs
+++ News_App/Article.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Article
 {
 	public string Title { get; set; }
 	public string Description { get; set; }
 	public string Url { get; set; }
+
+   
 }
+
 
 
--- News_App/Program.cs
+++ News_App/Program.cs
@@ -1,12 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Fetching news...");
 
+// Define the Article class
 Article article = new Article();
-
+// Set properties of the article
 article.Title = "Breaking News: C# 12 Released!";
 article.Description = "The latest version of C# has been released, bringing new features and improvements to the language.";
 article.Url = "https://www.example.com/news/csharp-12-released";
-
+// Display the article information
 Console.WriteLine($"Title: {article.Title}");
 Console.WriteLine($"Description: {article.Description}");
 Console.WriteLine($"URL: {article.Url}");
+
+
+// Create a list of articles
+List<Article> articles = new List<Article>();
+// Add the first article to the list
+articles.Add(new Article
+{
+    Title = "Tech Giants Announce New Collaboration",
+    Description = "Several major tech companies have announced a new collaboration to develop innovative technologies.",
+    Url = "https://www.example.com/news/tech-giants-collaboration"
+});
+// Add the second article to the list
+articles.Add(new Article
+{
+    Title = "Global Economy Shows Signs of Recovery",
+    Description = "Economic indicators suggest that the global economy is showing signs of recovery after a challenging period.",
+    Url = "https://www.example.com/news/global-economy-recovery"
+});
+
+foreach (var articleItem in articles)
+{
+    Console.WriteLine($"Title: {articleItem.Title}");
+    Console.WriteLine($"Description: {articleItem.Description}");
+    Console.WriteLine($"URL: {articleItem.Url}");
+}
+
